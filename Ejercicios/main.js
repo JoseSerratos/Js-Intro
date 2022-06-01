0f007d755e329745ca251f2dd7d5ef66c4ecdcb4
JoseSerratos <104475338+JoseSerratos@users.noreply.github.com>
2022-06-01 14:30:27 -0500

    Create main.js

---
+++ Ejercicios/main.js
@@ -0,0 +1,251 @@
+/*
+//Ejercicio1 
+var bellisimo= prompt ("eres bellisimo");
+console.log(bellisimo)
+
+if(bellisimo=="si")
+{
+    console.log("Ciertamente")
+}
+if (bellisimo=="no")
+{
+    console.log("No te creo")
+}
+if(bellisimo=="NO")
+{
+    console.log("No te creo")
+}
+
+*/
+
+
+/*
+//Ejercicio2
+var numero=prompt("Ingresa un numero");
+console.log(numero)
+
+if(numero%2==0)
+{
+    console.log( numero + " número es divisible entre 2")
+}
+
+else
+{
+    console.log( numero +" número no es divisible entre 2")
+}
+
+*/
+
+
+
+/*
+//Ejercicio3
+
+var numero=promt("Ingrese un numero")
+console.log(numero)
+
+    if(numero%2==0)
+{
+
+    alert("El numero es par")
+}
+
+*/
+
+/*
+//Ejercicio4
+
+var numero=prompt("Ingrese un numero")
+console.log(numero)
+
+if(numero==1000)
+{
+    alert("Ganaste un premio")
+}
+
+else {
+    alert("Lo siento sigue intentando")
+}
+
+*/
+
+
+
+
+/*
+//Ejercicio4
+
+var numero=prompt("Ingrese un numero")
+var numero2=prompt("Ingrese el numero2")
+console.log(numero)
+
+ if(numero>numero2)
+ {
+     alert("El numero " + numero +"Es mayor")
+ }
+
+ if(numero<numero2)
+ {
+     alert("El numero " + numero2 +"Es mayor")
+ }
+
+ */
+
+ /*
+//Ejericio6
+
+
+var numero=prompt("Ingrese un numero")
+var numero2=prompt("Ingrese el numero2")
+var numero3=prompt("Ingrese el numero3")
+console.log(numero)
+console.log(numero2)
+console.log(numero3)
+
+ if(numero>numero2 && numero>numero3)
+ {
+     console.log("El numero" + numero + "es mayor");
+ }
+
+
+ if(numero2>numero && numero2>numero3)
+ {
+     console.log("El numero "+ numero2 + "Es mayor");
+ }
+ 
+
+ if(numero3>numero && numero3>numero)
+ {
+     console.log("El numero"+ numero3 + "Es mayor");
+ }
+
+ if(numero == numero2)
+ {
+     console.log(" el numero 1 & 2 se repiten");
+ }
+
+
+ if(numero == numero3)
+ {
+     console.log("El numero 1 & 3 se repiten");
+ }
+
+
+ if(numero3 == numero2)
+ {
+     console.log("El numero 2 & 3 se repiten");
+ }
+
+ */
+
+
+
+ /*
+//Ejercicio7
+
+var dia=prompt("ingresa un dia de la semana")
+console.log(dia)
+
+
+if(dia == "Lunes" )
+{
+    console.log("Ingreso el dia Lunes");
+}
+
+if(dia == "Viernes" )
+{
+    console.log("Ingreso el dia Viernes");
+}
+
+if(dia == "Sabado" )
+{
+    console.log("Ingreso el dia Sabado");
+}
+
+
+if(dia == "Domingo" )
+{
+    console.log("Ingreso el dia Domingo");
+}
+
+else {
+    console.log("Error , ingrese Lunes , Viernes , Sabado o Domingo")
+}
+
+*/
+
+
+
+/*
+//Ejercicio8
+var calificacion=prompt("Ingrese su calificacion")
+console.log(calificacion)
+if(calificacion>10)
+{
+    alert("Error, Ingrese un numero del 1 al 10")
+}
+if(calificacion<6)
+{
+    alert("Esta Reprobado")
+}
+if(calificacion>=6 && calificacion<=8)
+{
+    alert("Regular")
+}
+
+if(calificacion==9 && calificacion<10)
+{
+    alert("Bien")
+}
+
+if(calificacion==10)
+{
+    alert("Excelente")
+}
+
+*/
+
+
+/*
+// Ejercicio9
+var helado=prompt("Su helado trenda topic? \n '1'.-Sin topic 50mxn \n '2'.-Oreo 10mxn \n '3'.-KitKat 15mxn \n '4'.-Brownie 20mxn")
+console.log(helado)
+
+if(helado==1)
+{
+    console.log(" El helado de oreo tiene un costo de : 50 MXN ")
+}if(helado==2)
+{
+ console.log(" El topping de oreo cuesta 10 MXN , " + " Total: " + (50+10) +" MXN " );
+}
+if(helado==3)
+{
+ console.log(" El topping de KitKat cuesta 15 MXN , " + " Total: " + (50+15) +" MXN " );
+}
+if(helado==4)
+{
+ console.log(" El topping de Brownie cuesta 20 MXN , " + " Total: " + (50+20) +" MXN " );
+}
+
+*/
+
+
+// Ejercicio10
+var estudio=prompt("Que desea Estudiar? \n '1'.-Master \n '2'.-Carrera \n '3'.-Course" )
+console.log(estudio)
+
+if(estudio==1)
+{
+    alert("Tiene un costo de 2999 MXN")
+}
+
+
+if(estudio==2)
+{
+    alert("Tiene un costo de 3999 MXN")
+}
+
+if(estudio==3)
+{
+    alert("Tiene un costo de 4999 MXN")
+}
